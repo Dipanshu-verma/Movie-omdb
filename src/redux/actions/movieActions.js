928e363737a11711895cc4c251191f781f12df67
Dipanshu-verma <vermadipanshu444@gmail.com>
2024-06-02 14:30:23 +0530

    done

--- src/redux/actions/movieActions.js
+++ src/redux/actions/movieActions.js
@@ -6,10 +6,9 @@ export const fetchMoviesFailure = (error) => ({ type: 'FETCH_MOVIES_FAILURE', pa
 export const selectMovie = (movie) => ({ type: 'SELECT_MOVIE', payload: movie });
 
 const apikey = process.env.REACT_APP_OMDB_API_KEY
- 
 export const fetchMovies = (query) => (dispatch) => {
     dispatch(fetchMoviesRequest());
-    axios.get(`http://www.omdbapi.com/?apikey=3352a333&s=${query}`)
+    axios.get(`http://www.omdbapi.com/?apikey=${apikey}&s=${query}`)
       .then(response => {
         const movies = response.data.Search;
         dispatch(fetchMoviesSuccess(movies));
@@ -22,7 +21,7 @@ export const fetchMovies = (query) => (dispatch) => {
   
 export const singleMovieDetail = (id) => (dispatch) => {
 
-    axios.get(`http://www.omdbapi.com/?i=${id}&apikey=3352a333`)
+    axios.get(`http://www.omdbapi.com/?i=${id}&apikey=${apikey}`)
       .then(response => {
         dispatch(selectMovie(response.data));
       })
